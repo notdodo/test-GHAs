9c429bce4216820968eda18ef7e095b1aa9be06f
Edoardo Rosa <6991986+notdodo@users.noreply.github.com>
2024-03-15 23:31:08 +0100

    test

--- auto-tag/main.py
+++ auto-tag/main.py
@@ -1,48 +1,37 @@
 #!/usr/bin/env python3
-
 import os
+import uuid
+from dataclasses import dataclass
+from enum import Enum
 
-# Config
-default_semvar_bump = os.getenv("DEFAULT_BUMP", "minor")
-default_branch = os.getenv("DEFAULT_BRANCH", os.getenv("GITHUB_BASE_REF"))
-with_v = os.getenv("WITH_V", "false").lower() == "true"
-release_branches = os.getenv("RELEASE_BRANCHES", "master,main").split(",")
-custom_tag = os.getenv("CUSTOM_TAG", "")
-source = os.getenv("SOURCE", ".")
-dryrun = os.getenv("DRY_RUN", "false").lower() == "true"
-git_api_tagging = os.getenv("GIT_API_TAGGING", "true").lower() == "true"
-initial_version = os.getenv("INITIAL_VERSION", "0.0.0")
-tag_context = os.getenv("TAG_CONTEXT", "repo")
-prerelease = os.getenv("PRERELEASE", "false").lower() == "true"
-suffix = os.getenv("PRERELEASE_SUFFIX", "beta")
-verbose = os.getenv("VERBOSE", "false").lower() == "true"
-major_string_token = os.getenv("MAJOR_STRING_TOKEN", "#major")
-minor_string_token = os.getenv("MINOR_STRING_TOKEN", "#minor")
-patch_string_token = os.getenv("PATCH_STRING_TOKEN", "#patch")
-none_string_token = os.getenv("NONE_STRING_TOKEN", "#none")
-branch_history = os.getenv("BRANCH_HISTORY", "compare")
-force_without_changes = os.getenv("FORCE_WITHOUT_CHANGES", "false").lower() == "true"
 
-os.chdir(os.path.join(os.getenv("GITHUB_WORKSPACE"), source))
+class BumpStrategy(Enum):
+    MAJOR: str = "major"
+    MINOR: str = "minor"
+    PATCH: str = "patch"
+    SKIP: str = "skip"
 
-# Print configuration
-print("*** CONFIGURATION ***")
-print(f"\tDEFAULT_BUMP: {default_semvar_bump}")
-print(f"\tDEFAULT_BRANCH: {default_branch}")
-print(f"\tWITH_V: {with_v}")
-print(f"\tRELEASE_BRANCHES: {release_branches}")
-print(f"\tCUSTOM_TAG: {custom_tag}")
-print(f"\tSOURCE: {source}")
-print(f"\tDRY_RUN: {dryrun}")
-print(f"\tGIT_API_TAGGING: {git_api_tagging}")
-print(f"\tINITIAL_VERSION: {initial_version}")
-print(f"\tTAG_CONTEXT: {tag_context}")
-print(f"\tPRERELEASE: {prerelease}")
-print(f"\tPRERELEASE_SUFFIX: {suffix}")
-print(f"\tVERBOSE: {verbose}")
-print(f"\tMAJOR_STRING_TOKEN: {major_string_token}")
-print(f"\tMINOR_STRING_TOKEN: {minor_string_token}")
-print(f"\tPATCH_STRING_TOKEN: {patch_string_token}")
-print(f"\tNONE_STRING_TOKEN: {none_string_token}")
-print(f"\tBRANCH_HISTORY: {branch_history}")
-print(f"\tFORCE: {force_without_changes}")
+
+@dataclass
+class Configuration:
+    BUMP_STRATEGY: BumpStrategy = BumpStrategy.SKIP
+    BRANCH: str = "main"  # $GITHUB_BASE_REF
+    PREFIX: str = ""
+    SUFFIX: str = ""
+
+
+def set_output(name, value):
+    with open(os.environ["GITHUB_OUTPUT"], "a") as fh:
+        print(f"{name}={value}", file=fh)
+
+
+def set_multiline_output(name, value):
+    with open(os.environ["GITHUB_OUTPUT"], "a") as fh:
+        delimiter = uuid.uuid1()
+        print(f"{name}<<{delimiter}", file=fh)
+        print(value, file=fh)
+        print(delimiter, file=fh)
+
+
+set_output("asdf", "asdf")
+set_multiline_output("asdfa", "ASdfsdafds\isdfjosdifjdfjj\jj")
