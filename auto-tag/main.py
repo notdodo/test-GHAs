22212939396e45e03ca996a2f99d568ec1884a8b
Edoardo Rosa <6991986+notdodo@users.noreply.github.com>
2024-03-17 22:53:56 +0100

    update

--- auto-tag/main.py
+++ auto-tag/main.py
@@ -88,7 +88,7 @@ def check_bump_strategy_since_last_tag(
         sha=last_available_tag.commit
     )  # TODO: this should be since=datetime
     for commit in last_commits_since_tag:
-        print(commit.commit, last_commits_since_tag, os.environ.get("GITHUB_SHA"))
+        print(commit.commit.sha, commit.commit.message, os.environ.get("GITHUB_SHA"))
         for strategy in strategies:
             if f"[#{strategy.lower()}]" in commit.commit.message:
                 return BumpStrategy(strategy)
