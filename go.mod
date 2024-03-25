445032b31a18dcc6e176bdd51b2443d310d22a89
Edoardo Rosa <6991986+notdodo@users.noreply.github.com>
2024-03-25 23:13:01 +0100

    test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/notdodo/test-GHAs
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+// comment without a to do
+func SomeFunc1() bool {
+	_ = 1 + 1
+	return true
+}
+
+func main() {
+	SomeFunc1()
+}
